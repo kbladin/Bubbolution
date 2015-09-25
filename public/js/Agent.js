07e920f95935d1d7c41b57036ddfa7e51d7c413f
kalbl <kallebladin@gmail.com>
2015-09-25 16:58:09 +0200

    Created some base classes.

---
+++ public/js/Agent.js
@@ -0,0 +1,50 @@
+//
+// Agent
+//
+
+function Agent () {
+    // State
+    this.posX;
+    this.posY;
+    this.direction;
+
+    genecode = {
+
+    }
+}
+
+// Atomic actions
+
+Agent.prototype.walk = function(dt, velocity) {
+
+};
+
+Agent.prototype.walkSideways = function(dt, velocity) {
+
+};
+
+Agent.prototype.turn = function(dt, angularVelocity) {
+
+};
+
+Agent.prototype.pickUp = function(dt) {
+
+};
+
+Agent.prototype.eat = function() {
+
+};
+
+Agent.prototype.hit = function() {
+
+};
+
+Agent.prototype.mate = function() {
+
+};
+
+// HighLevel actions
+
+Agent.prototype.functionName = function() {
+
+};
---
+++ public/js/Brain.js
@@ -0,0 +1,21 @@
+//
+// Brain
+//
+
+function Brain () {
+    
+}
+
+Brain.prototype.mutate = function(mutationRate) {
+	// 1. Mutate body
+	// 2. Mutate brain
+};
+
+Brain.prototype.mate = function(brainMate) {
+    // Create a child
+    
+    // 1. Mutate body
+	// 2. Mutate brain
+
+    return child;
+};
